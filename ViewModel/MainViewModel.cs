d24e8b07e5d9226fa7bd912342049ad749aa68a8
barususjn <160046222+barususjn@users.noreply.github.com>
2024-02-29 22:04:13 +0100

    MVVMCore added

---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using MVVMCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokeProofOrganizer.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public override void Initialize(object data)
+        {
+            
+        }
+
+        public override void OnBeforeClose()
+        {
+            
+        }
+    }
+}
